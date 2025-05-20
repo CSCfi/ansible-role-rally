3e88a293f7127041418164d85a3797121b6d1663
Antonio Delgado <adelgado@csc.fi>
2025-05-20 12:53:43 +0300

    Use systemd for purger

--- files/rally_verify_purger.py
+++ files/rally_verify_purger.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
 """ Python Script to purge old rally verify runs """
 
 from __future__ import print_function
@@ -17,10 +20,10 @@ PARSER = argparse.ArgumentParser(description='Purge old verify tests.')
 PARSER.add_argument('-p', dest='PATTERN', type=str,
                     default="cron",
                     help='grep pattern to match for')
-PARSER.add_argument('-s', dest='SAVENEWER', type=int,
+PARSER.add_argument('-s', dest='SAVE_NEWER', type=int,
                     default=90,
                     help='Save newer runs than this amount of days, default: 90')
-PARSER.add_argument('-n', dest='SAVEBEGIN', type=int,
+PARSER.add_argument('-n', dest='SAVE_BEGIN', type=int,
                     default=5,
                     help='Amount from beginning to save, default: 5')
 PARSER.add_argument('-d', dest='DEBUG', action='store_true',
@@ -29,15 +32,19 @@ PARSER.add_argument('-d', dest='DEBUG', action='store_true',
 PARSER.add_argument('-b', dest='BATCH', type=int,
                     default=5,
                     help='For every "modulus 5" verify that we purge, sleep 5 seconds')
+PARSER.add_argument('-c', dest='RALLY_CMD', type=int,
+                    default='rally',
+                    help='Rally command')
 
 
 ARGS = PARSER.parse_args()
 
 BATCH = ARGS.BATCH
 PATTERN = ARGS.PATTERN
-SAVENEWER = ARGS.SAVENEWER
-SAVEBEGIN = ARGS.SAVEBEGIN
+SAVE_NEWER = ARGS.SAVE_NEWER
+SAVE_BEGIN = ARGS.SAVE_BEGIN
 DEBUG = ARGS.DEBUG
+RALLY_CMD = ARGS.RALLY_CMD
 
 ####
 
@@ -45,9 +52,11 @@ NOW = datetime.now()
 
 # Grabs column 2(UUID) and 6(Started at)
 # NOTE: There is a rally python library we could use instead. If you do that, remember to sort
-P = subprocess.Popen("rally verify list|grep %s|cut -d '|' -f2,6|tail -n +%s" %
-		                   (PATTERN, SAVEBEGIN),
-                     shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()[0]
+P = subprocess.Popen(
+        f"{RALLY_CMD} verify list|grep {PATTERN}|cut -d '|' -f2,6|tail -n +{SAVE_BEGIN}",
+        shell=True,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE).communicate()[0]
 RALLY_VERIFY_LIST = P.split("\n")
 # Get rid of empty strings (because above we split on newline)
 RALLY_VERIFY_LIST2 = [x for x in RALLY_VERIFY_LIST if x]
@@ -66,7 +75,7 @@ def init_log():
 def delete_verifys(rally_verify_list, save_these_many_days):
     """ Delete verify runs
 	Input: list of lists: [ [ "verifyuuid", "started date" ], [], .. ]
-        Output: NA """
+    Output: NA """
 
     cnt = 0
 
@@ -79,8 +88,8 @@ def delete_verifys(rally_verify_list, save_these_many_days):
         age = NOW-date_sameasnow
         if age.days > save_these_many_days and uuid:
             # NOTE: There is a rally python library that this rally CLI tool uses
-            subprocess.Popen("rally verify delete --uuid %s" % uuid, shell=True)
-            LOG.warn("Deleted verify with UUID=%s because it was too old", uuid)
+            subprocess.Popen(f"{RALLY_CMD} verify delete --uuid {uuid}", shell=True)
+            LOG.warning("Deleted verify with UUID=%s because it was too old", uuid)
             cnt = cnt + 1
             if cnt % BATCH:
                 time.sleep(BATCH)
@@ -93,7 +102,7 @@ def delete_verifys(rally_verify_list, save_these_many_days):
 def safety_check(out):
     """ Exit script on errors
 	Input: list of lists: [ [ "verifyuuid", "started date" ], [], .. ]
-        Output: NA """
+    Output: NA """
 
     if not out:
         LOG.error("No verifys found in rally verify list, aborting")
@@ -101,4 +110,4 @@ def safety_check(out):
 
 LOG = init_log()
 safety_check(RALLY_VERIFY_LIST2)
-delete_verifys(RALLY_VERIFY_LIST2, SAVENEWER)
+delete_verifys(RALLY_VERIFY_LIST2, SAVE_NEWER)
